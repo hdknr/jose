f7499ac2466a2f7fd77a00acd00ba152dd2b6964
hdknr <gmail@hdknr.com>
2014-03-07 21:06:10 +0900

    README: fork of bozhu/AES-GCM-Python provides simple setup script.

--- README.rst
+++ README.rst
@@ -15,13 +15,26 @@ Most of them
 ECDSA, ECDH-ES
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
+- https://github.com/niccokunzmann/ecc
+
 .. code-block:: bash
 
     $ pip install ecc
 
-GCM
+PKCS#5 v2.0 PBKDF2 Module
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
 .. code-block:: bash
 
     $ pip install -e git+https://github.com/dlitz/python-pbkdf2.git#egg=pbkdf2
+
+128 bit AES GCM
+^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+
+- https://github.com/bozhu/AES-GCM-Python
+
+For convenience, there is a setup.py in hdknr fork :
+
+.. code-block:: bash
+
+    $ pip install -e git+https://github.com/hdknr/AES-GCM-Python#egg=aes_gcm
--- src/jose/jwa/tests/test_ec.py
+++ src/jose/jwa/tests/test_ec.py
@@ -3,7 +3,212 @@
 import unittest
 
 
-class TestEcKey(unittest.TestCase):
+class TestEcKeyEcc(unittest.TestCase):
+
+    def test_generate(self):
+        from ecc.Key import Key
+
+        pri = Key.generate(521)
+        self.assertTrue(isinstance(pri, Key))
+        self.assertTrue(pri.private())
+
+        pub = Key.decode(pri.encode())
+        self.assertTrue(isinstance(pub, Key))
+        self.assertFalse(pub.private())
+
+        msg = "hello, it's me."
+        sig = pri.sign(msg, hashfunc='sha512')  # default sha256
+        self.assertTrue(pub.verify(msg, sig, hashfunc='sha512'))
+
+    def test_dhZ(self):
+        from ecc.Key import Key
+
+        # Party V(recepient) declares a satic key pair
+        curve_bits = 521
+        v_stc = Key.generate(curve_bits)
+
+        # and advertise to Party U(Sender)
+        v_pub = Key.decode(v_stc.encode())
+
+        # Party U provides a ephemeral key
+        u_epk = Key.generate(v_pub._pub[0])
+
+        # Getting NIST Curve
+        from ecc.curves import get_curve
+        from ecc.elliptic import mulp
+
+        _curve = lambda bits:  dict(
+            zip(('bits', 'p', 'N', 'a', 'b', 'G'),
+                get_curve(bits)))
+        # Compute ECDH
+        _dhZ = lambda crv, pub, pri: mulp(
+            crv['a'], crv['b'], crv['p'], pub, pri)[0]
+
+        # Party U compute
+        u_crv = _curve(u_epk._priv[0])
+        shared_secret_u = _dhZ(u_crv, v_pub._pub[1], u_epk._priv[1])
+
+        # Party V recive Epemeral Public Key
+        v_epk = Key.decode(u_epk.encode())
+        # Party V compute
+        shared_secret_v = _dhZ(u_crv, v_epk._pub[1], v_stc._priv[1])
+
+        # Secrete Agreeed!
+        self.assertEqual(shared_secret_u, shared_secret_v)
+
+    def test_ecdh(self):
+        '''
+https://tools.ietf.org/html/draft-ietf-jose-json-web-algorithms-23#appendix-C
+        '''
+
+        v_stc_material = {
+            "kty": "EC",
+            "crv": "P-256",
+            "x": "gI0GAILBdu7T53akrFmMyGcsF3n5dO7MmwNBHKW5SV0",
+            "y": "SLW_xSffzlPWrHEVI30DHM_4egVwt3NQqeUD7nMFpps",
+            "d": "0_NxaRPUMQoAJt50Gz8YiTr8gRTwyEaCumd-MToTmIo"
+        }
+
+        u_epk_material = {
+            "kty": "EC",
+            "crv": "P-256",
+            "x": "weNJy2HscCSM6AEDTDg04biOvhFhyyWvOHQfeF_PxMQ",
+            "y": "e8lnCO-AlStT-NJVX-crhB7QRYhiix03illJOVAOyck",
+            "d": "VEmDZpDXXK8p8N0Cndsxs924q6nS1RXFASRl6BfUqdw"
+        }
+
+        import re
+        from jose.utils import base64
+        _to_pub = lambda km: (
+            int(re.search(r"P-(\d+)$", "P-256").group(1)),
+            (base64.long_from_b64(km['x']),
+             base64.long_from_b64(km['y']))
+        )
+        _to_pri = lambda km: (
+            int(re.search(r"P-(\d+)$", "P-256").group(1)),
+            base64.long_from_b64(km['d'])
+        )
+
+        from ecc.Key import Key
+        v_stc = Key(
+            public_key=_to_pub(v_stc_material),
+            private_key=_to_pri(v_stc_material)
+        )
+
+        v_pub = Key.decode(v_stc.encode())
+
+        # Party U provides a ephemeral key
+        u_epk = Key(
+            public_key=_to_pub(u_epk_material),
+            private_key=_to_pri(u_epk_material)
+        )
+
+        # Getting NIST Curve
+        from ecc.curves import get_curve
+        from ecc.elliptic import mulp
+
+        _curve = lambda bits:  dict(
+            zip(('bits', 'p', 'N', 'a', 'b', 'G'),
+                get_curve(bits)))
+
+        # Compute ECDH
+        _dhZ = lambda crv, pub, pri: mulp(
+            crv['a'], crv['b'], crv['p'], pub, pri)[0]
+
+        # Party U compute
+        u_crv = _curve(u_epk._priv[0])
+        shared_secret_u = _dhZ(u_crv, v_pub._pub[1], u_epk._priv[1])
+
+        from Crypto.Util.number import long_to_bytes
+        from math import ceil
+
+        block_size = int(ceil(u_epk._priv[0] / 8.0))
+        # bit number(512 )  / 8 -> octets
+        Zu = long_to_bytes(shared_secret_u, block_size)
+
+        Z_jwa = [158, 86, 217, 29, 129, 113, 53,
+                 211, 114, 131, 66, 131, 191, 132,
+                 38, 156, 251, 49, 110, 163, 218,
+                 128, 106, 72, 246, 218, 167, 121,
+                 140, 254, 144, 196]
+
+        self.assertEqual([ord(i) for i in Zu], Z_jwa)
+
+        # Other Information used in Concat KDF
+        # AlgorithmID || PartyUInfo || PartyVInfo || SuppPubInfo
+        from struct import pack
+        _otherInfo = lambda alg, pu, pv, klen: ''.join([
+            pack("!I", len(alg)),
+            alg,
+            pack("!I", len(pu)),
+            pu,
+            pack("!I", len(pv)),
+            pv,
+            pack("!I", klen),
+        ])
+
+        oi_u = _otherInfo(
+            "A128GCM",
+            "Alice",
+            "Bob",
+            16 * 8,     # A128GCM
+        )
+
+        oi_jwa = [
+            0, 0, 0, 7,
+            65, 49, 50, 56, 71, 67, 77,
+            0, 0, 0, 5,
+            65, 108, 105, 99, 101,
+            0, 0, 0, 3,
+            66, 111, 98,
+            0, 0, 0, 128]
+
+        self.assertEqual([ord(i) for i in oi_u], oi_jwa)
+
+        # Coccat KDF : NIST defines SHA256
+        from Crypto.Hash import SHA256
+
+        def _ConcatKDF(Z, dkLen, otherInfo,
+                       digest_method=SHA256):
+            _src = lambda counter_bytes: "".join([
+                counter_bytes, Z, otherInfo])
+
+            from math import ceil
+            from struct import pack
+
+            dkm = b''   # Derived Key Material
+            counter = 0
+            klen = int(ceil(dkLen / 8.0))
+            while len(dkm) < klen:
+                counter += 1
+                counter_b = pack("!I", counter)
+                dkm += digest_method.new(_src(counter_b)).digest()
+
+            return dkm[:klen]
+
+        _derived_key_u = _ConcatKDF(Zu, 16 * 8, oi_u)
+
+        # Party V recive Epemeral Public Key
+        v_epk = Key.decode(u_epk.encode())
+        Zv = long_to_bytes(
+            _dhZ(u_crv, v_epk._pub[1], v_stc._priv[1]),
+            block_size)
+
+        _derived_key_v = _ConcatKDF(Zv, 16 * 8, oi_u)
+
+        self.assertEqual(_derived_key_u, _derived_key_v)
+
+        kd_jwa = [
+            86, 170, 141, 234, 248, 35, 109, 32,
+            92, 34, 40, 205, 113, 167, 16, 26]
+
+        self.assertEqual([ord(i) for i in _derived_key_u], kd_jwa)
+
+        self.assertEqual("VqqN6vgjbSBcIijNcacQGg",
+                         base64.base64url_encode(_derived_key_u))
+
+
+class TestEcKeyEcdsa(unittest.TestCase):
     def test_generate(self):
         from ecdsa import SigningKey, NIST521p
 
