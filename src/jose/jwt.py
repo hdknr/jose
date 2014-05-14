bd2e2a8666e97e668fb558aa9836fd6f8a682365
hdknr <gmail@hdknr.com>
2014-05-14 09:27:26 +0900

    Jwt epoch time to datetime with tzinfo

--- src/jose/jwt.py
+++ src/jose/jwt.py
@@ -1,6 +1,14 @@
 from jose.base import BaseObject
 from jose.utils import _BD
 from jose.crypto import Crypto
+from datetime import datetime
+import time
+import pytz 
+from dateutil.tz import tzlocal
+import re
+
+_ZONE = dict(utc=pytz.utc, local=tzlocal())
+_TIME_FIELD = re.compile('^(?P<name>.+)_dt_(?P<zone>.+)$')
 
 
 class Jwt(BaseObject):
@@ -10,7 +18,7 @@ class Jwt(BaseObject):
         aud=None,       # Audient: list(str) or str
         exp=None,
         nbf=None,
-        iat=None,
+        iat=None,       # import time; time.gmtime()
         jti=None,
     )
     _excludes = ['verified']
@@ -18,6 +26,32 @@ class Jwt(BaseObject):
     def __init__(self, verified=False, **kwargs):
         super(Jwt, self).__init__(**kwargs)
         self.verified = verified
+
+    def __getattr__(self, name):
+        _names = _TIME_FIELD.search(name)
+        _names = _names and _names.groupdict()
+        _val = _names and getattr(self, _names['name'])
+        _zone  = _names and _ZONE.get(_names['zone']) 
+
+        if _val and _zone:
+            return datetime.fromtimestamp(_val).replace(tzinfo=pytz.utc).astimezone(_zone)
+
+        return self.__getattribute__(name)
+    
+
+    def is_available(self, me, epoch=None):
+        epoch = epoch or int(time.time())   # UTC
+        if self.exp and self.exp < epoch:
+            return False
+        if self.nbf and self.nbf > epoch:
+            return False
+        if isinstance(self.aud, basestring) and self.aud != me:
+            return False
+        elif isinstance(self.aud, list) and me not in self.aud:
+            return False
+
+        return True
+            
 
     @classmethod
     def header(cls, token):
