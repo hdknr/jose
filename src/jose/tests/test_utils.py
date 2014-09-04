c36f5a91b01a2b83bc1712aeb1af364786c2e881
hdknr <gmail@hdknr.com>
2014-09-04 10:30:57 +0900

    utils tested.

--- src/jose/tests/test_utils.py
+++ src/jose/tests/test_utils.py
@@ -6,6 +6,9 @@ from jose.utils import base64
 
 class TestBase64(unittest.TestCase):
     def test_base64(self):
+        '''
+        nose2 jose.tests.test_utils.TestBase64.test_base64
+        '''
         self.assertEquals('QWxpY2U', base64.base64url_encode('Alice'))
         self.assertEquals('Qm9i', base64.base64url_encode('Bob'))
 
